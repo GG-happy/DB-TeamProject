30de1ae8fdf1ef1491a1978306de78f34d9ab74b
JHLgithuber <88205863+JHLgithuber@users.noreply.github.com>
2024-11-30 18:34:41 +0900

    start.spring.io

---
+++ DBP_financeMgmt_Team11/DBP_financeMgmt_Team11/src/main/java/edu/deu/dbprogramming/DBP_financeMgmt_Team11/DbpFinanceMgmtTeam11Application.java
@@ -0,0 +1,13 @@
+package edu.deu.dbprogramming.DBP_financeMgmt_Team11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbpFinanceMgmtTeam11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbpFinanceMgmtTeam11Application.class, args);
+	}
+
+}
---
+++ DBP_financeMgmt_Team11/DBP_financeMgmt_Team11/src/test/java/edu/deu/dbprogramming/DBP_financeMgmt_Team11/DbpFinanceMgmtTeam11ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.deu.dbprogramming.DBP_financeMgmt_Team11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbpFinanceMgmtTeam11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
